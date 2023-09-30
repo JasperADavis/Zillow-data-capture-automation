c7325979cdc5274b68c76cd1872afb485c51a1b5
Jasper Davis <66934862+JasperADavis@users.noreply.github.com>
2023-09-30 15:58:06 -0400

    Fix main.py
    Wrong version of main.py uploaded

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ import time
 import itertools
 
 
-zillow_url = "https://www.zillow.com/homes/for_rent/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22mapBounds%22%3A%7B%22west%22%3A-122.90711440039063%2C%22east%22%3A-121.95954359960938%2C%22south%22%3A37.4265788314989%2C%22north%22%3A38.12236702051197%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%7D"
-google_form_url = "https://docs.google.com/forms/d/e/1FAIpQLScxSwZpD-sn-lvjZCRYZO09bpHa5ATR5-2NAcu-uedg6-03aQ/viewform?usp=sf_link"
+zillow_url = "" # Replace with Zillow search URL
+google_form_url = "" # Replace with Google Form URL
 
 chrome_driver_path = "/Users/Jasper/Developer/ChromeDriver/chromedriver"
 options = webdriver.ChromeOptions()
